8e8cb6391c819f35032f0c036ea79508ff024b6f
Cardenet <100084868+Cardenet@users.noreply.github.com>
2023-03-20 10:57:14 +0100

    Update users.sql

--- users.sql
+++ users.sql
@@ -1,23 +1,23 @@
 CREATE TABLE users (
-  id INT PRIMARY KEY,
-  username VARCHAR(50) UNIQUE,
-  name VARCHAR(50),
-  surname VARCHAR(50),
-  email VARCHAR(100) UNIQUE,
-  pass VARCHAR(100),
-  role INT,
-  FOREIGN KEY (role) REFERENCES role(role)
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  username VARCHAR(50) UNIQUE NOT NULL,
+  name VARCHAR(50) NOT NULL,
+  surname VARCHAR(50) NOT NULL,
+  email VARCHAR(100) UNIQUE NOT NULL,
+  pass VARBINARY(100) NOT NULL,
+  role VARCHAR(50) NOT NULL,
+  FOREIGN KEY (role) REFERENCES role(role_name)
 );
 
 CREATE TABLE role (
-  id INT PRIMARY KEY,
-  role VARCHAR(50)
+  id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  role_name VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE results (
-  id INT PRIMARY KEY,
-  query VARCHAR(100),
-  result VARCHAR(100),
-  username VARCHAR(50),
+  id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  query VARCHAR(100) NOT NULL ,
+  result VARCHAR(100) NOT NULL ,
+  username VARCHAR(50) NOT NULL,
   FOREIGN KEY (username) REFERENCES users(username)
 );

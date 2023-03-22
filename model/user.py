0dffdba4cdadfbb605e1cc74e6c584d03f15cbb6
Arnau Albert <arnaualbert@pop-os.localdomain>
2023-03-22 16:45:37 +0100

    login

--- model/user.py
+++ model/user.py
@@ -1,14 +1,15 @@
 class User:
-    def __init__(self, username, name, surname, email, password, role):
+    def __init__(self,id, username, name, surname, email, pass_hash, role_id):
+        self.id = id
         self.username = username
         self.name = name
         self.surname = surname
         self.email = email
-        self.password = password
-        self.role = role
+        self.pass_hash = pass_hash
+        self.role_id = role_id
 
     def __str__(self):
-        return f'Username: {self.username}, Name: {self.name}, Surname: {self.surname}, Email: {self.email}, Role: {self.role}'
+        return f'id: {self.id}, Username: {self.username}, Name: {self.name}, Surname: {self.surname}, Email: {self.email},Pass: {self.pass_hash} Role: {self.role_id}'
 
     def to_dict(self):
         return {
@@ -16,6 +17,6 @@ class User:
             'name': self.name,
             'surname': self.surname,
             'email': self.email,
-            'password': self.password,
-            'role': self.role
+            'password': self.pass_hash,
+            'role': self.role_id
         }

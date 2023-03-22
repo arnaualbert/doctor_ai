1c3beb825fd11dc5c6e78279acfce14ca1ff40fc
Arnau Albert <arnaualbert@pop-os.localdomain>
2023-03-22 19:00:42 +0100

    login.py comment

--- model/login.py
+++ model/login.py
@@ -2,26 +2,32 @@ import mysql.connector
 import model.user as user
 from typing import Union
 
+### Connect to the database
 conexion = mysql.connector.connect(
     host="localhost",
-    user="root",
-    password="",
+    user="admin",
+    password="1234",
     database="doctor_ai"
 )
 
 
 def login(username, password) -> Union[bool, user.User]:
-        #create a cursor
-        cursor = conexion.cursor()
-        # Verify if the user is correct
-        cursor.execute("SELECT * FROM users WHERE username=%s AND pass_hash=%s", (username, password))
-        resultado = cursor.fetchone()
-        if resultado is None:
-            return False
-        else:
-            return user.User(*resultado)
+    """Log in the user
+    @param username: username
+    @param password: password
+    """
+    #create a cursor
+    cursor = conexion.cursor()
+    # Verify if the user is correct
+    cursor.execute("SELECT * FROM users WHERE username=%s AND pass_hash=%s", (username, password))
+    resultado = cursor.fetchone()
+    if resultado is None:
+        return False
+    else:
+        return user.User(*resultado)
 
 def register(username,name,surname,email,password,role_id):
+    """Register a new user"""
     cursor = conexion.cursor()
     cursor.execute("INSERT INTO users (username,name,surname,email,pass_hash,role_id) VALUES (%s,%s,%s,%s,%s,%s)",(username,name,surname,email,password,role_id))
     conexion.commit()

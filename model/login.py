c44566c54808354e04d6833899f475d3d8355f9d
Arnau Albert <arnaualbert@pop-os.localdomain>
2023-03-24 16:44:23 +0100

    login and database

--- model/login.py
+++ model/login.py
@@ -1,16 +1,6 @@
-import mysql.connector
 import model.user as user
 from typing import Union
 import model.database as databases
-
-
-## Connect to the database
-conexion = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="",
-    database="doctor_ai"
-)
 
 data = databases.Database("localhost","root","","doctor_ai")
 
@@ -20,12 +10,6 @@ def login(username: str, password:str) -> Union[bool, user.User]:
     @param password: password
     @return: user or False
     """
-    #create a cursor
-    # cursor = conexion.cursor()
-    # # Verify if the user is correct
-    # cursor.execute("SELECT * FROM users WHERE username=%s AND pass_hash=%s", (username, password))
-    # resultado = cursor.fetchone()
-    # resultado = data.query("SELECT * FROM users WHERE username=%s AND pass_hash=%s", (username, password))
     resultado = data.query(f'SELECT * FROM users WHERE username="{username}" AND pass_hash="{password}"')
     print(resultado)
     if resultado is None:
@@ -35,12 +19,14 @@ def login(username: str, password:str) -> Union[bool, user.User]:
 
 def register(username: str,name: str,surname: str,email: str,password: str,role_id: int) -> bool:
     """Register a new user"""
-    cursor = conexion.cursor()
-    cursor.execute(f"SELECT * FROM users WHERE username = '{username}'")
-    resultado = cursor.fetchone()
+    # cursor = conexion.cursor()
+    # cursor.execute(f"SELECT * FROM users WHERE username = '{username}'")
+    # resultado = cursor.fetchone()
+    resultado = data.query(f"SELECT * FROM users WHERE username = '{username}'")
     if resultado is None:
-        cursor.execute("INSERT INTO users (username,name,surname,email,pass_hash,role_id) VALUES (%s,%s,%s,%s,%s,%s)",(username,name,surname,email,password,role_id))
-        conexion.commit()
+        data.query(f"INSERT INTO users (username,name,surname,email,pass_hash,role_id) VALUES ('{username}','{name}','{surname}','{email}','{password}',{role_id})")
+        # cursor.execute("INSERT INTO users (username,name,surname,email,pass_hash,role_id) VALUES (%s,%s,%s,%s,%s,%s)",(username,name,surname,email,password,role_id))
+        # conexion.commit()
         return True
     else:
         return False

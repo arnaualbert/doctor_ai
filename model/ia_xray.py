a0c38153b474382e488d2d490191d24ce01c58d6
Arnau Albert <arnaualbert@pop-os.localdomain>
2023-04-12 19:27:59 +0200

    asd

---
+++ model/ia_xray.py
@@ -0,0 +1,45 @@
+from tensorflow import keras
+from keras.layers import Input, Lambda, Dense, Flatten
+from keras.models import Model
+from keras.applications.vgg16 import VGG16
+from keras.applications.vgg16 import preprocess_input
+from keras.preprocessing import image
+from keras.preprocessing.image import ImageDataGenerator
+from keras.models import Sequential
+import numpy as np
+from glob import glob
+import matplotlib.pyplot as plt
+from keras.preprocessing.image import ImageDataGenerator
+import tensorflow as tf
+from keras.models import load_model
+from keras.models import load_model
+from keras.preprocessing import image
+from keras.applications.vgg16 import preprocess_input
+import numpy as np
+
+class IAML:
+    def __init__(self):
+        pass
+
+    def ask():
+
+        model=load_model('chest_xray.h5')
+
+        img=tf.keras.utils.load_img('/home/arnaualbert/Desktop/pneumonia_dl/chest_xray_pneumonia/chest_xray/val/NORMAL/NORMAL2-IM-1427-0001.jpeg',target_size=(224,224))
+
+        x = tf.keras.utils.img_to_array(img)
+
+        x=np.expand_dims(x, axis=0)
+
+        img_data=preprocess_input(x)
+
+        classes=model.predict(img_data)
+        print(classes)
+
+        result=int(classes[0][0])
+        print(result)
+
+        if result==0:
+            return "Result is Pneumonia"
+        else:
+            return "Result is Normal"

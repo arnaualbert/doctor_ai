74e97f8e7cc95c6859f784990128290aab6c68ac
Cardeenet <carde602@gmail.com>
2023-04-15 11:54:43 +0200

    making test

---
+++ files_for_testing/test_dna_protein/test_dna_rna.py
@@ -0,0 +1,3 @@
+import subprocess
+
+subprocess.run(["/home/luis/Escritorio/doctor_ai/dna_rna","/home/luis/Escritorio/doctor_ai/files_for_testing/test_dna_protein/sequence.fasta"])
---
+++ model/scrips.py
@@ -0,0 +1,5 @@
+# def dna_rna(file):
+#     script ="dna_rna"
+import  os 
+path = os.getcwd()
+print(path)

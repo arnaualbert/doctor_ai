301b2216ca78a302f9090e07c515343ef4d0fa07
arnaualbert <arnaualbert2003@gmail.com>
2023-05-02 20:14:35 +0200

    clear code

--- 1_team_members/arnau/img_rec.py
+++ 1_team_members/arnau/img_rec.py
@@ -1,70 +1,70 @@
-from tensorflow import keras
-from keras.layers import Input, Lambda, Dense, Flatten
-from keras.models import Model
-from keras.applications.vgg16 import VGG16
-from keras.applications.vgg16 import preprocess_input
-from keras.preprocessing import image
-from keras.preprocessing.image import ImageDataGenerator
-from keras.models import Sequential
-import numpy as np
-from glob import glob
-import matplotlib.pyplot as plt
+# from tensorflow import keras
+# from keras.layers import Input, Lambda, Dense, Flatten
+# from keras.models import Model
+# from keras.applications.vgg16 import VGG16
+# from keras.applications.vgg16 import preprocess_input
+# from keras.preprocessing import image
+# from keras.preprocessing.image import ImageDataGenerator
+# from keras.models import Sequential
+# import numpy as np
+# from glob import glob
+# import matplotlib.pyplot as plt
 
-IMAGE_SIZE = [224, 224]
+# IMAGE_SIZE = [224, 224]
 
-train_path = 'chest_xray_pneumonia/chest_xray/train'
-valid_path = 'chest_xray_pneumonia/chest_xray/val'
+# train_path = 'chest_xray_pneumonia/chest_xray/train'
+# valid_path = 'chest_xray_pneumonia/chest_xray/val'
 
-vgg = VGG16(input_shape=IMAGE_SIZE + [3], weights='imagenet', include_top=False)
+# vgg = VGG16(input_shape=IMAGE_SIZE + [3], weights='imagenet', include_top=False)
 
-for layer in vgg.layers:
-    layer.trainable = False
+# for layer in vgg.layers:
+#     layer.trainable = False
 
-folders = glob('chest_xray_pneumonia/chest_xray/train/*')
-x = Flatten()(vgg.output)
-
-
-prediction = Dense(len(folders), activation='softmax')(x)
-# create a model object
-model = Model(inputs=vgg.input, outputs=prediction)
-# view the structure of the model
-model.summary()
-
-model.compile(
-  loss='categorical_crossentropy',
-  optimizer='adam',
-  metrics=['accuracy']
-)
-
-from keras.preprocessing.image import ImageDataGenerator
-
-train_datagen = ImageDataGenerator(rescale = 1./255,
-                                   shear_range = 0.2,
-                                   zoom_range = 0.2,
-                                   horizontal_flip = True)
-
-test_datagen = ImageDataGenerator(rescale = 1./255)
-
-# Make sure you provide the same target size as initialied for the image size
-training_set = train_datagen.flow_from_directory('chest_xray_pneumonia/chest_xray/train',
-                                                 target_size = (224, 224),
-                                                 batch_size = 10,
-                                                 class_mode = 'categorical')
-
-test_set = test_datagen.flow_from_directory('chest_xray_pneumonia/chest_xray/test',
-                                            target_size = (224, 224),
-                                            batch_size = 10,
-                                            class_mode = 'categorical')
+# folders = glob('chest_xray_pneumonia/chest_xray/train/*')
+# x = Flatten()(vgg.output)
 
 
+# prediction = Dense(len(folders), activation='softmax')(x)
+# # create a model object
+# model = Model(inputs=vgg.input, outputs=prediction)
+# # view the structure of the model
+# model.summary()
 
-r = model.fit(
-  training_set,
-  validation_data=test_set,
-  epochs=1,
-  steps_per_epoch=len(training_set),
-  validation_steps=len(test_set)
-)
+# model.compile(
+#   loss='categorical_crossentropy',
+#   optimizer='adam',
+#   metrics=['accuracy']
+# )
+
+# from keras.preprocessing.image import ImageDataGenerator
+
+# train_datagen = ImageDataGenerator(rescale = 1./255,
+#                                    shear_range = 0.2,
+#                                    zoom_range = 0.2,
+#                                    horizontal_flip = True)
+
+# test_datagen = ImageDataGenerator(rescale = 1./255)
+
+# # Make sure you provide the same target size as initialied for the image size
+# training_set = train_datagen.flow_from_directory('chest_xray_pneumonia/chest_xray/train',
+#                                                  target_size = (224, 224),
+#                                                  batch_size = 10,
+#                                                  class_mode = 'categorical')
+
+# test_set = test_datagen.flow_from_directory('chest_xray_pneumonia/chest_xray/test',
+#                                             target_size = (224, 224),
+#                                             batch_size = 10,
+#                                             class_mode = 'categorical')
+
+
+
+# r = model.fit(
+#   training_set,
+#   validation_data=test_set,
+#   epochs=1,
+#   steps_per_epoch=len(training_set),
+#   validation_steps=len(test_set)
+# )
 
 
 
@@ -74,7 +74,7 @@ r = model.fit(
 import tensorflow as tf
 from keras.models import load_model
 
-model.save('brain.h5')  ### chest_xray.h5
+# model.save('brain.h5')  ### chest_xray.h5
 
 from keras.models import load_model
 
@@ -84,25 +84,25 @@ from keras.applications.vgg16 import preprocess_input
 
 import numpy as np
 
-# model=load_model('chest_xray.h5')
+model=load_model('chest_xray.h5')
 
-# img=tf.keras.utils.load_img('/home/arnaualbert/Desktop/pneumonia_dl/chest_xray_pneumonia/chest_xray/val/NORMAL/NORMAL2-IM-1427-0001.jpeg',target_size=(224,224))
+img=tf.keras.utils.load_img('/home/arnaualbert/Desktop/pneumonia_dl/chest_xray_pneumonia/chest_xray/test/PNEUMONIA/person99_bacteria_473.jpeg',target_size=(224,224))
 
-# x = tf.keras.utils.img_to_array(img)
+x = tf.keras.utils.img_to_array(img)
 
-# x=np.expand_dims(x, axis=0)
+x=np.expand_dims(x, axis=0)
 
-# img_data=preprocess_input(x)
+img_data=preprocess_input(x)
 
-# classes=model.predict(img_data)
-# print(classes)
+classes=model.predict(img_data)
+print(classes)
 
-# result=int(classes[0][0])
-# print(result)
+result=int(classes[0][0])
+print(result)
 
-# if result==0:
-#     print("Person is Affected By PNEUMONIA")
-# else:
-#     print("Result is Normal")
+if result==0:
+    print("Person is Affected By PNEUMONIA")
+else:
+    print("Result is Normal")
 
 
--- model/scripts.py
+++ model/scripts.py
@@ -1,5 +1,27 @@
 import  os 
 import subprocess
+import os
+import model.login as logins
+import model.user as users
+import model.iaxray as ia
+import model.upload as upload
+from typing import Union
+from datetime import datetime
+from pathlib import Path
+import shutil
+import glob
+import re
+from celery import Celery, Task
+from multiprocessing import Process
+from concurrent.futures import ThreadPoolExecutor
+from concurrent.futures import ThreadPoolExecutor
+from threading import Thread
+executor = ThreadPoolExecutor(5)
+########
+from multiprocessing import Pool
+from random import *
+import subprocess
+import multiprocessing
 
 path = os.getcwd()
 
@@ -12,8 +34,23 @@ def dna_protein(file):
 def extract_cds(file):
     subprocess.run([path+"/extract_cds", file])
 
-def random_seq(length):
-    subprocess.run([path+"/random", length])
+# def random_seq(length):
+def random_sequence_task(number,user_id):
+    """Execute the random sequence program and return the new filename"""
+    id = randint(1, 9999999)
+    ids = str(id)
+    file_up = "dna.fasta"
+    query = "random_sequence"
+    a = upload.upload_results(id,query,user_id)   
+    subprocess.run(["./random", number])
+    new_filename = re.sub(r'\.fasta$', ids+'random.fasta', file_up)
+    os.rename(file_up, new_filename)
+    b = upload.update_date(id,new_filename,user_id)              
+    print(a)     
+    print(b)
+    print(new_filename)
+    return new_filename
+    # subprocess.run([path+"/random", length])
 
 def local_alignment(file1,file2):
     subprocess.run([path+"/local_alignment", file1, file2])
@@ -23,3 +60,80 @@ def global_alignment(file1,file2):
 
 def gb_to_fasta(file):
     subprocess.run([path+"/genbankToFastaV3", file])
+
+
+def complementary_task(fasta,user_id):
+    id = randint(1, 9999999)
+    out_name = f"{id}complementary.fasta"
+    query = "complementary"
+    upload.upload_results(id,query,user_id)
+    subprocess.run(["./complementary",fasta,out_name])
+    upload.update_date(id,out_name,user_id)
+
+
+def split_fasta_task(fasta, user_id,start,end):
+    query = "split_fasta"
+    id = randint(1, 9999999)
+    upload.upload_results(id,query,user_id)
+    subprocess.run(["./split",fasta,start,end])
+    file_up = f"output_{start}_{end}.fasta"
+    new_filename = re.sub(r'\.fasta$', str(id)+'output_'+start+'_'+end+'.fasta', file_up)
+    os.rename(file_up, new_filename)
+    upload.update_date(id,new_filename,user_id)
+
+
+def genbank_to_fasta(fullroute,user_id):
+    id = randint(1,9999999)
+    ids = str(id)
+    file_up = Path('gb_to_fasta.fasta')
+    query = "gb_to_fasta"
+    upload.upload_results(id,query,user_id)  
+    subprocess.run(["./genbankToFastaV3",fullroute])
+    new_filename = Path(ids+'gb_to_fasta.fasta')
+    asd = file_up.rename(new_filename)
+    upload.update_date(id,asd,user_id)
+
+
+def cdsextract_task(fullroute,user_id):
+    id = randint(1,9999999)
+    ids = str(id)
+    file_up = Path('resultado.fasta')
+    query = "cds_extract"
+    upload.upload_results(id,query,user_id)  
+    subprocess.run(["./extract_cds",fullroute])
+    new_filename = Path(ids+'resultado.txt')
+    asd = file_up.rename(new_filename)
+    upload.update_date(id,asd,user_id)
+
+
+def dna_to_rna(fullroute,filename,user_id,id):
+    query = "dna_to_rna"
+    upload.upload_results(id,query,user_id)
+    subprocess.run(["./dna_rna",fullroute])
+    new_filename = re.sub(r'\.fasta$', '_modified.fasta', filename)
+    file_up = "dnatorna/"+new_filename
+    upload.update_date(id,file_up,user_id)
+
+def dna_to_protein(fullroute,filename,user_id,id):
+    query = "dnaprotein"
+    upload.upload_results(id,query,user_id)
+    subprocess.run(["./dna_protein",fullroute])
+    new_filename = re.sub(r'\.fasta$', '_protein.fasta', filename)
+    file_up = "dnaprotein/"+new_filename
+    upload.update_date(id,file_up,user_id)
+
+
+def local(fasta1_filepath, fasta2_filepath, match, mismatch, gap,user_id):
+        # Execute the local aligment program
+    id = randint(1,9999999)
+    ids = str(id)
+    file_up = "alignment_result.txt"
+    query = "local_alignment"
+    upload.upload_results(id,query,user_id)
+    print("running local alignment")
+    subprocess.run(["./local_alignment",fasta1_filepath, fasta2_filepath, match, mismatch, gap])
+    print("finished local alignment")
+    new_filename = re.sub(r'\.txt$',ids+'alignment_result.txt', file_up)
+    print(new_filename)
+    os.rename(file_up, new_filename)
+    upload.update_date(id,new_filename,user_id)

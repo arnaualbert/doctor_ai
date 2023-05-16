741c27a10a6f1cddc39359d67412b90c52649ec9
Cardenet <carde602@gmail.com>
2023-05-16 17:11:40 +0200

    history

--- static/orderByDate.js
+++ static/orderByDate.js
@@ -1,36 +1,12 @@
 $(document).ready(function() {
-    var startDateOrder = 1; // 1 para orden ascendente, -1 para orden descendente
     var finishDateOrder = 1; // 1 para orden ascendente, -1 para orden descendente
-
-    $("#start-date-header").on("click", function() {
-      startDateOrder *= -1; // Cambia el orden al hacer clic
-    
-    $("#table tbody").html(
-      $("#table tbody tr").toArray().sort(function(a, b) {
-        var dateA = new Date($(a).find("td:nth-child(4)").text());
-        var dateB = new Date($(b).find("td:nth-child(4)").text());
-
-      return startDateOrder * (dateA - dateB);
-    })
-  );
-  
-
-  // Actualiza el estilo visual del encabezado para mostrar el orden actual
-  if (startDateOrder === 1) {
-    $(this).removeClass("desc");
-    $(this).addClass("asc");
-  } else {
-    $(this).removeClass("asc");
-    $(this).addClass("desc");
-  }
-});
 $("#finish-date-header").on("click", function() {
     finishDateOrder *= -1; // Cambia el orden al hacer clic
     
     $("#table tbody").html(
-      $("#table tbody tr").toArray().sort(function(a, b) {
-        var dateC = new Date($(a).find("td:nth-child(5)").text());
-        var dateD = new Date($(b).find("td:nth-child(5)").text());
+      $("#table tbody tr").toArray().sort(function(c, d) {
+        var dateC = new Date($(c).find("td:nth-child(5)").text());
+        var dateD = new Date($(d).find("td:nth-child(5)").text());
 
       return finishDateOrder * (dateC - dateD);
     })
@@ -56,4 +32,5 @@ $("#filter-input").on("keyup", function() {
       $(this).toggle(filterMatch);
     });
   });
+  
   });

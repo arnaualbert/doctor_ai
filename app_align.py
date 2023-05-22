818444440eb514d2651f70a1657b5ef7c1145db2
arnaualbert <arnaualbert2003@gmail.com>
2023-05-22 17:22:26 +0200

    validate fasta

--- app_align.py
+++ app_align.py
@@ -116,9 +116,11 @@ def local_alignment():
             # return "running"
             return render_template('local_aligment.html',message="Running")
         else:
+            message = validate.validate_local_aligment(fasta1local, fasta2local, match, mismatch, gap,gapLeft,gapUp)
             os.remove(fasta1local)
             os.remove(fasta2local)
-            return render_template('local_aligment.html',message="Please fill all the fields with the correct format")
+            return render_template('local_aligment.html',message=message)
+            # return render_template('local_aligment.html',message="Please fill all the fields with the correct format")
         
 #Blosum Local aligment
 # #-------------------------------------------
